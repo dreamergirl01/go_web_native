a1ec57abbb98ab3fd7b14a83c4445782e2998c4d
dreamergirl01 <rahma01twins@gmail.com>
2025-01-25 21:54:48 +0700

    edit data

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -6,6 +6,7 @@ import (
 	"go_web_native/models"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 var validation = libraries.NewValidation()
@@ -67,7 +68,56 @@ func Add(response http.ResponseWriter, request *http.Request) {
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		//variabel untuk mengambil id yang ada pada (edit) index.html
+		queryString := request.URL.Query()
+		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
 
+		var pasien entities.Pasien
+		PasienModel.Find(id, &pasien)
+
+		data := map[string]interface{}{
+			"pasien": pasien,
+		}
+
+		//tampilkan data
+		temp, err := template.ParseFiles("views/pasien/edit.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, data)
+	}else if request.Method == http.MethodPost{
+		//proses database
+		request.ParseForm()
+
+		// meneriima inputan dari form yang sudah dibuat (views/pasien/add.html),kemudian inputan yang sudah diterima akan disimpan pada NamaLengkap di struc pasien yang sudah dibuat pada entities pasien
+		var pasien entities.Pasien
+		pasien.ID, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHp = request.Form.Get("no_hp")
+
+		var data = make(map[string]interface{})
+
+		vErrors := validation.Struct(pasien)
+
+		if vErrors != nil {
+			data["pasien"] = pasien
+			data["validation"] = vErrors
+		}else{
+			data["pesan"] = "Data Pasien Berhasil Diedit"
+			//proses simpan data ke database
+			PasienModel.Update(pasien)
+		}
+		
+		//setelah proses simpan data ke database telah berhasil kembalikan view untuk tambah data
+		temp, _ := template.ParseFiles("views/pasien/edit.html")
+		temp.Execute(response, data)
+	}
 }
 
 func Delete(response http.ResponseWriter, request *http.Request) {
--- models/pasienmodel.go
+++ models/pasienmodel.go
@@ -75,3 +75,27 @@ func (p *PasienModel) Create(pasien entities.Pasien) bool {
 
 	return lastInsertId > 0
 }
+
+//untuk proses edit
+func (p* PasienModel) Find(id int64, pasien *entities.Pasien) error{
+	return p.conn.QueryRow("select * from pasien where id = ?", id).Scan(
+		&pasien.ID, 
+		&pasien.NamaLengkap,
+		&pasien.NIK,
+		&pasien.JenisKelamin,
+		&pasien.TempatLahir,
+		&pasien.TanggalLahir,
+		&pasien.Alamat,
+		&pasien.NoHp)
+}
+func (p* PasienModel) Update(pasien entities.Pasien) error {
+	_, err := p.conn.Exec(
+		"update pasien set nama_lengkap = ?, nik = ?, jenis_kelamin = ?, tempat_lahir = ?, tanggal_lahir = ?, alamat = ?, no_hp = ? where id = ?",
+		pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHp, pasien.ID)
+	
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
